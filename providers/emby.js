4cfdf88edf08d6947f5059de5d65e3bb356b2c08
lielayt <lielayt@gmail.com>
2026-03-09 10:55:39 +0200

    attempt 2

--- providers/emby.js
+++ providers/emby.js
@@ -7,16 +7,17 @@ function getStreams(tmdbId, mediaType, season, episode) {
 
     return login().then(token => {
 
-        return searchItem(token, tmdbId).then(item => {
+        return searchItem(token, tmdbId, season, episode).then(item => {
 
             if (!item) return [];
 
-            const streamUrl =
-                EMBY_SERVER +
-                "/Videos/" +
-                item.Id +
-                "/stream?static=true&api_key=" +
-                token;
+            let streamUrl = `${EMBY_SERVER}/Videos/${item.Id}/stream?static=true&api_key=${token}`;
+
+            // For TV shows, append season/episode if needed
+            if (mediaType === "tv" && season != null && episode != null) {
+                const ep = item.Seasons?.find(s => s.IndexNumber === season)?.Episodes?.find(e => e.IndexNumber === episode);
+                if (ep) streamUrl = `${EMBY_SERVER}/Videos/${ep.Id}/stream?static=true&api_key=${token}`;
+            }
 
             return [{
                 name: "Emby",
@@ -32,37 +33,30 @@ function getStreams(tmdbId, mediaType, season, episode) {
 }
 
 function login() {
+    const headers = {
+        "Content-Type": "application/json",
+        "X-Emby-Authorization": 'Emby Client="EmbyWeb", Device="Android TV", DeviceId="androidtv-1234", Version="1.0.0"'
+    };
 
-    return fetch(EMBY_SERVER + "/Users/AuthenticateByName", {
+    return fetch(`${EMBY_SERVER}/Users/AuthenticateByName`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            Username: USERNAME,
-            Pw: PASSWORD
-        })
+        headers,
+        body: JSON.stringify({ Username: USERNAME, Pw: PASSWORD })
     })
     .then(res => res.json())
-    .then(data => data.AccessToken);
+    .then(data => {
+        if (!data.AccessToken) throw new Error("No AccessToken returned");
+        return data.AccessToken;
+    });
 }
 
-function searchItem(token, tmdbId) {
+function searchItem(token, tmdbId, season, episode) {
 
-    const url =
-        EMBY_SERVER +
-        "/Users/" +
-        USER_ID +
-        "/Items?AnyProviderIdEquals=Tmdb." +
-        tmdbId +
-        "&api_key=" +
-        token;
-
+    const url = `${EMBY_SERVER}/Users/${USER_ID}/Items?AnyProviderIdEquals=Tmdb.${tmdbId}&api_key=${token}`;
     return fetch(url)
         .then(res => res.json())
         .then(data => data.Items && data.Items[0]);
 }
 
-if (typeof module !== "undefined") {
-    module.exports = { getStreams };
-}
+// Export for Nuvio
+if (typeof module !== "undefined") module.exports = { getStreams };
