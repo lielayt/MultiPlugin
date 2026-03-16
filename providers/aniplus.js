ba4e8880dae81486226f6731248126ff74660566
lielayt <lielayt@gmail.com>
2026-03-16 23:50:37 +0200

    test

--- providers/aniplus.js
+++ providers/aniplus.js
@@ -1,6 +1,6 @@
 /**
  * aniplus - Built from src/aniplus/
- * Generated: 2026-03-16T21:41:02.602Z
+ * Generated: 2026-03-16T21:49:47.132Z
  */
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __commonJS = (cb, mod) => function __require() {
@@ -143,7 +143,8 @@ var require_extractor = __commonJS({
           "User-Agent": "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
           "Referer": "https://anipluspro.upn.one/",
           "Origin": "https://anipluspro.upn.one"
-        }
+        },
+        mimeType: "video/x-matroska"
       };
     }
     module2.exports = { toStream: toStream2 };
--- src/aniplus/extractor.js
+++ src/aniplus/extractor.js
@@ -13,7 +13,8 @@ function toStream(episode) {
                       "User-Agent": "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
                       "Referer" : "https://anipluspro.upn.one/",
                       "Origin" : "https://anipluspro.upn.one"
-                 }
+                 },
+        mimeType: "video/x-matroska"
     };
 }
 
